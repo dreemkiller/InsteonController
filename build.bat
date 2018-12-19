899e92f6aec3f924bca2e5c8f4d47da8edd4e134
Derek Miller <derek.miller@arm.com>
2018-12-19 14:41:41 -0600

    Added a copy to build.bat because -f no longer works

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 arm-none-eabi-ld -r -b binary Floorplan_first.bmp -o ./floorplan_first.o
 arm-none-eabi-ld -r -b binary Floorplan_second.bmp -o ./floorplan_second.o
-mbed compile -m LPC546XX -t GCC_ARM -f
+mbed compile -m LPC546XX -t GCC_ARM
+copy .\BUILD\LPC546XX\GCC_ARM\InsteonController.bin d:\
